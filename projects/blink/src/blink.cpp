0bc3ce7cf73bac9d24afc32e0467b0884f529414
Justin Mammarella <jmamma@gmail.com>
2024-12-08 11:34:00 +1100

    Fix Makefile again. SdFat/bench.cpp is working

--- projects/blink/src/blink.cpp
+++ projects/blink/src/blink.cpp
@@ -4,15 +4,18 @@ void setup() {
   Serial.begin(115200);  // Start Serial
   Serial.println("Starting up...");
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(9, OUTPUT);
   Serial.println("LED pin configured");
 }
 
 void loop() {
   Serial.println("LED HIGH");
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
+  digitalWrite(9, HIGH);
+  delay(500);
   Serial.println("LED LOW");
   digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  digitalWrite(9, LOW);
+  delay(500);
 }
 
